64ebff282924ba004bc19d2d4b47d93eaefe61a7
root <root@pgo.jmiao.net>
2016-07-30 03:20:42 -0700

    more devel

--- run.php
+++ run.php
@@ -1,6 +1,6 @@
 <?php
 include("./config.php");
-
+while (1==1) {
 $pdb = new SQLite3($path);
 $pdb->busyTimeout(5000);
 $db = new SQLite3("alarm.db");
@@ -17,8 +17,10 @@ fclose($p_file);
 
 watchPokemon(131);
 watchPokemon(143);
-watchPokemon(21);
-print_r($watch);
+watchPokemon(150);
+
+echo "\nWatching for pokemon\n";
+
 foreach($watch as $pid) {
 
 do {
@@ -27,12 +29,21 @@ try {
 $res = $pdb->query('SELECT * FROM `pokemon` WHERE `pokemon_id` = '. $pid . ' ORDER BY `disappear_time` DESC LIMIT 1;');
 if ($res_array = $res->fetchArray(SQLITE3_ASSOC)) {
 	$lastSeen = strtotime($res_array["disappear_time"] . "UTC");
-echo "Last " . $pokemon[$pid] . " seen: " . date("F j, Y, g:i a", strtotime($res_array["disappear_time"] . "UTC")) . ".";
+echo "Last " . $pokemon[$pid] . " expired: " . date("F j, Y, g:i a", strtotime($res_array["disappear_time"] . "UTC")) . ".";
 if ($lastSeen > time()) {
 	echo " Not expired";
-	foreach($notify as $sendTo) {
-	exec("curl -k -X POST 'https://api.twilio.com/2010-04-01/Accounts/" . $twilioSid . "/Messages.json' --data-urlencode 'To=+" . $sendTo . "' --data-urlencode 'From=+" . $twilioFrom . "' --data-urlencode 'Body=" . $pokemon[$pid] . " is around. Expires in " . round(($lastSeen - time())/60,2) . " minutes.' -u '" . $twilioSid . ":" . $twilioAuth . "'");
+	$resExists = $db->query('SELECT * FROM `pokemon` WHERE `encounter` = \'' . $res_array["encounter_id"] . '\';');
+	if ($resExists->fetchArray()) {
+		echo ". Already notified";
+	} else {
+
+				echo ". Not notified.";
+			foreach($notify as $sendTo) {
+	exec("curl -sS -k -X POST 'https://api.twilio.com/2010-04-01/Accounts/" . $twilioSid . "/Messages.json' --data-urlencode 'To=+" . $sendTo . "' --data-urlencode 'From=+" . $twilioFrom . "' --data-urlencode 'Body=" . $pokemon[$pid] . " is around. Expires in " . round(($lastSeen - time())/60,2) . " minutes.' -u '" . $twilioSid . ":" . $twilioAuth . "'");
+	$db->exec("INSERT INTO `pokemon` (`encounter`) VALUES ('" . $res_array["encounter_id"] . "');");
 	}
+	}
+
 } else {
 	echo " Expired";
 	foreach($notify as $sendTo) {
@@ -40,6 +51,8 @@ if ($lastSeen > time()) {
 	}
 }
 echo "\n";
+} else {
+	echo $pokemon[$pid] . " not seen in db.\n";
 }
 } catch (Error $e) {
 	echo "error" . $e->getMessage();
@@ -52,6 +65,15 @@ echo "\n";
 
 }
 
+
+
+
+$pdb->close();
+unset($pdb);
+$db->close();
+unset($db);
+sleep(5);
+}
 
 function watchPokemon($pid) {
 	global $watch;
@@ -71,8 +93,3 @@ function watchPokemon($pid) {
 
 }
 */
-$pdb->close();
-unset $pdb;
-$db->close();
-unset $db;
-
