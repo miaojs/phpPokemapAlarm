6ef002aefa05efd3d1afe201779c0a97def02d2a
root <root@pgo.jmiao.net>
2016-09-22 16:51:31 -0700

    forgot to delete more test code..

--- run.php
+++ run.php
@@ -43,7 +43,7 @@ if ($lastSeen > time()) {
 				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 				$loc = json_decode(curl_exec($ch), true);
 				curl_close($ch);
-	exec("curl -sS -k -X POST 'https://api.twilio.com/2010-04-01/Accounts/" . $twilioSid . "/Messages.json' --data-urlencode 'To=+" . $sendTo . "' --data-urlencode 'From=+" . $twilioFrom . "' --data-urlencode 'Body=!!TEST IGNORE!!" . $pokemon[$pid] . " is around. Expires in " . round(($lastSeen - time())/60,2) . " minutes. \n\n" . $loc["results"][0]['formatted_address'] . "\nhttps://www.google.com/maps/place/" . $res_array['latitude'] . "," . $res_array['longitude'] . "' -u '" . $twilioSid . ":" . $twilioAuth . "'");
+	exec("curl -sS -k -X POST 'https://api.twilio.com/2010-04-01/Accounts/" . $twilioSid . "/Messages.json' --data-urlencode 'To=+" . $sendTo . "' --data-urlencode 'From=+" . $twilioFrom . "' --data-urlencode 'Body=" . $pokemon[$pid] . " is around. Expires in " . round(($lastSeen - time())/60,2) . " minutes. \n\n" . $loc["results"][0]['formatted_address'] . "\nhttps://www.google.com/maps/place/" . $res_array['latitude'] . "," . $res_array['longitude'] . "' -u '" . $twilioSid . ":" . $twilioAuth . "'");
 	$db->exec("INSERT INTO `pokemon` (`encounter`) VALUES ('" . $res_array["encounter_id"] . "');");
 	}
 	}
